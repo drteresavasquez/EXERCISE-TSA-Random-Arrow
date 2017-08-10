95c5669f7fa0c3cd8bd7ee9e8b8141cd9dc4cc64
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-08-10 17:01:20 -0500

    assignment completed

---
+++ random.js
@@ -0,0 +1,17 @@
+"use strict";
+console.log("random.js, YO!");
+
+let images = {
+	0 : "images/left.png",
+	1: "images/right.png",
+};
+
+let clickWindow = document.getElementById("randomArrow");
+
+window.onclick = windowClick;
+
+function windowClick(){
+	console.log("You clicked the Window!");
+	let num = Math.floor(Math.random() * 2)
+	clickWindow.innerHTML = `<img src="../${images[num]}"/>`
+}
